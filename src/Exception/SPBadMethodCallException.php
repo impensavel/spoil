5d44a9199e7b10b809504193ff23d0ec34957f83
Quetzy Garcia <qgarcia@wearearchitect.com>
2015-10-16 11:31:19 +0100

    feat(*): add new exception classes

---
+++ src/Exception/SPBadMethodCallException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of the SPOIL library.
+ *
+ * @author     Quetzy Garcia <quetzyg@impensavel.com>
+ * @copyright  2014-2015
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE.md file that was distributed
+ * with this source code.
+ */
+
+namespace Impensavel\Spoil\Exception;
+
+class SPBadMethodCallException extends SPRuntimeException
+{
+}
