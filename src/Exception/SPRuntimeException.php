ba1530296e03ba5093620d7f6095d285c05cff8a
Quetzy Garcia <qgarcia@wearearchitect.com>
2015-10-16 11:30:14 +0100

    feat(*): remove Guzzle specific error handling, rename class to SPRuntimeException and move to own namespace

---
+++ src/Exception/SPRuntimeException.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of the SPOIL library.
+ *
+ * @author     Quetzy Garcia <quetzyg@impensavel.com>
+ * @copyright  2014-2015
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE.md file that was distributed
+ * with this source code.
+ */
+
+namespace Impensavel\Spoil\Exception;
+
+use RuntimeException;
+
+class SPRuntimeException extends RuntimeException
+{
+    /**
+     * Get the previous Exception message
+     *
+     * @access  public
+     * @return  string|null
+     */
+    public function getPreviousMessage()
+    {
+        $previous = $this->getPrevious();
+
+        return $previous ? $previous->getMessage() : null;
+    }
+}
